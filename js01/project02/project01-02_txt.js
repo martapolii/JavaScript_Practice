e851fb04ab096d3798a3dc04898c5c3a5f9ccbc0
martapolii <mpolish3@my.centennialcollege.ca>
2024-05-27 12:28:15 -0400

    Declared and intilialized variables in an external .js file, linekd this file to my html, created a table and wrote script to load variable values into the table.

--- js01/project02/project01-02_txt.js
+++ js01/project02/project01-02_txt.js
@@ -2,10 +2,19 @@
     Chapter 1
     Hands-On Project 1-2
 
-    Author: 
-    Date:   
+    Author: Marta Polishchuk
+    Date:   May 27, 2024
 
     Filename: project01-02.js
 */
 
 //define variables for service name and service speed
+// variables declared using "var"
+var service1Name = "Basic"
+var service2Name = "Express"
+var service3Name = "Extreme"
+var service4Name = "Ultimate"
+var service1Speed = "0 Mbps"
+var service2Speed = "100 Mbps"
+var service3Speed = "500 Mbps"
+var service4Speed = "1 Gig"
