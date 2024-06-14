292d82e5c10776ec19b6ecdde954f49a949c1b7f
martapolii <mpolish3@my.centennialcollege.ca>
2024-06-14 11:06:59 -0400

    HOP 1-4: Creating dynamically-filled forms, input determined by radio button selection.

--- js01/project04/project01-04_txt.js
+++ js01/project04/project01-04_txt.js
@@ -2,11 +2,18 @@
     Chapter 1
     Hands-On Project 1-4
 
-    Author: 
-    Date:   
+    Author: Marta Polishchuk
+    Date:   June 14, 2024
 
     Filename: project01-04.js
 */
 
 //define variables for home and work addresses
-
+var homeStreet = "1 Main St"
+var homeCity = "Sicilia"
+var homeState = "MA"
+var homeCode = "02103"
+var workStreet = "15 Oak Ln."
+var workCity = "CentralCity"
+var workState = "MA"
+var workCode = "02104"
