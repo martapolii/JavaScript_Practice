4026d91a0d4668dd827264c5f0ccac960d32ab7d
martapolii <mpolish3@my.centennialcollege.ca>
2024-06-14 12:15:26 -0400

    Added extra comments to code.

--- js02/project01/project02-01_txt.js
+++ js02/project01/project02-01_txt.js
@@ -3,9 +3,25 @@
       Project 02-01
 
       Celsius <-> Farenheit Coverter
-      Author: 
-      Date:   
+      Author: Marta Polishchuk
+      Date:   June 14, 2024
 
       Filename: project02-01.js
- */
+*/
+
+// #3 create a function:
+function FarenheitToCelcius(degree)
+{     // using operators to calculate the value for a variable
+      degree = (degree - 32)/1.8
+      //returning the value of this variable as the output of this function (can then use this variable's value elsewhere)
+      return degree;
+}
+
+// #4
+function CelciusToFarenheit (degree)
+{
+      degree = (degree * 1.8) + 32
+      return degree;
+}
+
 
