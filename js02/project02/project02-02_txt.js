d76940f8d31607057e62678a01caa2d6125854dc
martapolii <mpolish3@my.centennialcollege.ca>
2024-06-17 09:32:12 -0400

    HOP 2-2: Verified whether all fields were filled in on a form with a conditional operator.

--- js02/project02/project02-02_txt.js
+++ js02/project02/project02-02_txt.js
@@ -3,9 +3,20 @@
       Project 02-02
 
       Application to test for completed form
-      Author: 
-      Date:   
+      Author: Marta Polishchuk
+      Date:   June 17, 2024 
 
       Filename: project02-02.js
  */
- 
+
+function verifyForm()
+{
+let name = document.getElementById('name').value;
+let email = document.getElementById('email').value;
+let phone = document.getElementById('phone').value;
+
+//conditional ooperator: condition? trueValue: false Value;
+name && email && phone != "" ? window.alert("Thank you!"): window.alert("Please fill in all fields");
+}   // if all 3 variables are not equal to an empty string, then trur (form is filled out), if any of them are an empty string then condition is not fulfilled, = false and form is not filed out fully
+
+document.getElementById('submit').onclick = verifyForm;
