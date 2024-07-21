686503b3c97613813bfd35049c4878feabf342e7
marta <m.polishchuk@hotmail.ca>
2024-07-21 17:39:48 -0400

    HOP 2-3: Create application that uses event handlers and mouseover/mouseout events to respond to the movement of a mouse over a page object.

--- js02/project03/project02-03_txt.js
+++ js02/project03/project02-03_txt.js
@@ -3,8 +3,26 @@
       Project 02-03
 
       Application to return the shape of a clicked object
-      Author: 
-      Date:   
+      Author: Marta Polishchuk
+      Date:   June 21, 2024  
 
       Filename: project02-03.js
  */
+
+      // added on mouseover event handler to element with id "square"
+      document.getElementById("square").addEventListener("mouseover", function() {document.getElementById("feedback").innerHTML = "You're hovering over the square."} ); //using an anonymous function to display the proper feedback message
+
+      //^ this makes a message pop-up that says "you are hovering over the square", but the message doesn't go away
+      // need to add a mouseout event handler that displays nothing (an empty text string) when mouse is no longer hovering over the object 
+      document.getElementById("square").addEventListener("mouseout", function() {document.getElementById("feedback").innerHTML = " "} );
+
+      // repeat above for the triangle
+      document.getElementById("triangle").addEventListener("mouseover", function() {document.getElementById("feedback").innerHTML = "You're hovering over the triangle."});
+
+      document.getElementById("triangle").addEventListener("mouseout", function () {document.getElementById("feedback").innerHTML =" "});
+
+      //repeat above for the circle
+      document.getElementById("circle").addEventListener("mouseover", function() {document.getElementById("feedback").innerHTML = "You're hovering over the circle."});
+
+      document.getElementById("circle").addEventListener("mouseout", function() {document.getElementById("feedback").innerHTML = " "});
+
