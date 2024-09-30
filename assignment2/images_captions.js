06b41c367e2de6fbbf9272da6b99f46864182089
martapolii <mpolish3@my.centennialcollege.ca>
2024-09-30 01:50:51 -0400

    Assignment 2: Slideshow, Lightbox, Overlay

---
+++ assignment2/images_captions.js
@@ -0,0 +1,23 @@
+// Title of slideshow:
+let lightboxTitle = "Gallery";
+
+//names of the image files, stored in an array:
+let imgFiles =["photo01.jpg", "photo02.jpg", "photo03.jpg", "photo04.jpg", "photo05.jpg", "photo06.jpg", "photo07.jpg", "photo08.jpg", "photo09.jpg", "photo10.jpg", "photo11.jpg", "photo12.jpg"]
+
+//captions for each image, also stored in an array:
+let imgCaptions = new Array(12);
+imgCaptions[0]="Death - Love (oil on canvas, 152cm x 152cm, 2024)";
+imgCaptions[1]="EASTER (acrylic, white gold on board, 12cm x 122cm, 2018)"; 
+imgCaptions[2]="i am (acrylic on board, 122cm x 122cm, 2018)"; 
+imgCaptions[3]="Do not disturb my circles! (oil on canvas, 122cm x 122cm, 2016)"; 
+imgCaptions[4]="Do not disturb my circles (oil on canvas, 122cm x 122cm, 2016)";
+imgCaptions[5]="Ukrainian Caryatids (acrylic on canvas, 122cm x 122cm, 2015)";
+imgCaptions[6]="HOPE (acrylic on canvas, 122cm x 122cm, 2015)";
+imgCaptions[7]="FAITH (acrylic on canvas, 122cm x 122cm, 2015)";
+imgCaptions[8]="Garden of Ukraine (acrylic on canvas, 92cm x 122cm, 2015)";
+imgCaptions[9]="MARIA (oil and 23kt.gold on canvas, 122cm x 122cm, 2014)";
+imgCaptions[10]="My Ukraine (oil on canvas, 76cm x 76cm, 2007)";
+imgCaptions[11]="Blood Transfusion Station (oil on canvas, 122cm x 122cm, 2012)";
+
+// Count of images in the slideshow stored in this variable: (for image counter)
+let imgCount = imgFiles.length;
